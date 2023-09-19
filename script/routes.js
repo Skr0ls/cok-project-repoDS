fa2517db55df291f68c50e9d7e4d4f0b9e4b2a8c
Vladimir Kosickij <vladimir.kosickij@gmail.com>
2023-09-19 21:53:20 +0300

    IMages...

--- script/routes.js
+++ script/routes.js
@@ -8,17 +8,11 @@ const routes = [
     getTemplate: (params) => '../modules/tourism/tourism.html'
   },
   {
-    path: '/clothes/:belts',
-    getTemplate: (params) => '../modules/clothes/belts/belts.html'
+    path: '/clothes/belts',
+    getTemplate: (params) => '../modules/belts/belts.html'
   },
   {
-    path: '/clothes/:shoulders',
-    getTemplate: (params) => '../modules/clothes/shoulders/shoulders.html'
+    path: '/clothes/shoulders',
+    getTemplate: (params) => '../modules/shoulders/shoulders.html'
   },
-  /* Пример рута с параметром
-  {
-    path: '/products/:productId',
-    getTemplate: (params) => `<h1>Product ${params.productId}</h1>`,
-  },
-  */
 ];
